5af5ca7f25a9d9c7bdd451b79d2f6d944979a5ed
Moe <muhtasim1023@gmail.com>
2026-07-18 16:47:21 -0400

    merged data into single dataframe

--- main.py
+++ main.py
@@ -2,6 +2,14 @@
 
 
 def main():
+    series_map = {
+        "policy_interest_rate": "V39079",
+        "cpi_total": "V41690973"
+    }
+
+    for values in series_map.values():
+        print(values)
+
     return 0
 
 
