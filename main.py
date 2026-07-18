8eb51c8c7964604d570286dfac6d56db766c8b5a
Moe <muhtasim1023@gmail.com>
2026-07-18 14:25:42 -0400

    best to do this analysis with a jupyter notebook to avoid repetitive API calls

--- main.py
+++ main.py
@@ -1,22 +1,12 @@
-import pandas as pd
-import numpy as np
-from pyvalet import ValetInterpreter
 
-def data_frame_display_config():
-    pd.set_option('display.max_columns', None)
-    pd.set_option('display.max_rows', None)
-    pd.set_option('display.max_colwidth', None)
-    pd.set_option('display.width', None)
 
-def main():
-    data_frame_display_config()
 
-    vi = ValetInterpreter()
-    series_df = vi.list_series()
-    print(series_df[["name", "description"]])
+def fetch_boc_data():
+    #vi = ValetInterpreter()
 
+    print(series_map.values())
     return 0
 
 
 if __name__ == '__main__':
-    main()
+    fetch_boc_data()
