e4c691168a3d2bf555582dcc2285018c81b928e8
Moe <muhtasim1023@gmail.com>
2026-07-18 15:22:51 -0400

    default state for main.py and new libraries added to project

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
 
 
 
-def fetch_boc_data():
-    #vi = ValetInterpreter()
-
-    print(series_map.values())
+def main():
     return 0
 
 
 if __name__ == '__main__':
-    fetch_boc_data()
+    main()
